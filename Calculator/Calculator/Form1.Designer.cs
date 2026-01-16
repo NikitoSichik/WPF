15c0868776fecd382782b52e15fd12046b37c379
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-01-16 18:32:10 +0300

    not end

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -28,33 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.plus = new System.Windows.Forms.Button();
+            this.rovno = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // num1
             // 
-            this.button1.Location = new System.Drawing.Point(468, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.num1.Location = new System.Drawing.Point(141, 146);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(75, 23);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(141, 90);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(41, 146);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(75, 23);
+            this.plus.TabIndex = 2;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // rovno
+            // 
+            this.rovno.Location = new System.Drawing.Point(41, 190);
+            this.rovno.Name = "rovno";
+            this.rovno.Size = new System.Drawing.Size(75, 23);
+            this.rovno.TabIndex = 3;
+            this.rovno.Text = "=";
+            this.rovno.UseVisualStyleBackColor = true;
+            this.rovno.Click += new System.EventHandler(this.rovno_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.rovno);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.num1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button rovno;
     }
 }
 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -12,9 +12,48 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        string cur_num = "";
+        string operation = "";
+        double first_num = 0;
+        double second_num = 0;
+        bool new_num = false;
         public Form1()
         {
             InitializeComponent();
+            textBox.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (new_num == false)
+            {
+                cur_num += btn.Text;
+                first_num = first_num * 10 + int.Parse(btn.Text);
+            }
+            else
+            {
+                cur_num += btn.Text;
+                second_num = first_num * 10 + int.Parse(btn.Text);
+            }
+            textBox.Text = cur_num;
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if (new_num == false)
+            {
+                Button btn = (Button)sender;
+                operation = btn.Text;
+                cur_num += btn.Text;
+                new_num = true;
+            }
+
+        }
+
+        private void rovno_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
