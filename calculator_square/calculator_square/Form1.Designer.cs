2c112ea5444ff37f95cbd5f7c13db127c0f6f0ff
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-01-24 21:48:16 +0300

    end

--- calculator_square/calculator_square/Form1.Designer.cs
+++ calculator_square/calculator_square/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.conclusion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(12, 54);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(100, 20);
+            this.num1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "square",
+            "rectangle"});
+            this.comboBox1.Location = new System.Drawing.Point(219, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "none";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(12, 80);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 20);
+            this.num2.TabIndex = 4;
+            // 
+            // conclusion
+            // 
+            this.conclusion.AutoSize = true;
+            this.conclusion.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.conclusion.Location = new System.Drawing.Point(100, 202);
+            this.conclusion.Name = "conclusion";
+            this.conclusion.Size = new System.Drawing.Size(0, 73);
+            this.conclusion.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(352, 284);
+            this.Controls.Add(this.conclusion);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label conclusion;
     }
 }
 
--- calculator_square/calculator_square/Form1.cs
+++ calculator_square/calculator_square/Form1.cs
@@ -12,9 +12,44 @@ namespace calculator_square
 {
     public partial class Form1 : Form
     {
+        int width;
+        int height;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "rectangle")
+            {
+                width = int.Parse(num1.Text);
+                height = int.Parse(num2.Text);
+                conclusion.Text = (height * width).ToString();
+
+            }
+            if(comboBox1.Text == "square")
+            {
+                width = int.Parse(num1.Text);
+                conclusion.Text = (width * width).ToString();
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "square")
+            {
+                button1.Text = "square";
+                num1.Visible = true;
+                num2.Visible = false;
+            }
+            if (comboBox1.Text == "rectangle")
+            {
+                button1.Text = "rectangle";
+                num2.Visible = true;
+                num1.Visible = true;
+            }
         }
     }
 }
