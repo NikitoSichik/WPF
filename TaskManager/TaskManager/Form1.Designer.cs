8ae8c0c77d84c37128fde6e3325dd119a52be30e
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-01-27 11:12:27 +0300

    not key, not

--- TaskManager/TaskManager/Form1.Designer.cs
+++ TaskManager/TaskManager/Form1.Designer.cs
@@ -32,16 +32,16 @@
             this.btn_add = new System.Windows.Forms.Button();
             this.btn_remove = new System.Windows.Forms.Button();
             this.Clear = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(182, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btn_add
             // 
@@ -51,6 +51,7 @@
             this.btn_add.TabIndex = 1;
             this.btn_add.Text = "add";
             this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
             // 
             // btn_remove
             // 
@@ -60,6 +61,7 @@
             this.btn_remove.TabIndex = 2;
             this.btn_remove.Text = "remove";
             this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
             // 
             // Clear
             // 
@@ -69,32 +71,22 @@
             this.Clear.TabIndex = 3;
             this.Clear.Text = "Clear";
             this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
             // 
-            // label1
+            // listBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(346, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(185, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 268);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.Clear);
             this.Controls.Add(this.btn_remove);
             this.Controls.Add(this.btn_add);
@@ -112,8 +104,7 @@
         private System.Windows.Forms.Button btn_add;
         private System.Windows.Forms.Button btn_remove;
         private System.Windows.Forms.Button Clear;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- TaskManager/TaskManager/Form1.cs
+++ TaskManager/TaskManager/Form1.cs
@@ -16,5 +16,33 @@ namespace TaskManager
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text)) {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Focus();
+                textBox1.Clear();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(textBox1.Text);
+            if(listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
     }
 }
