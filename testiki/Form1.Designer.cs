0f33931fadc68742267ca96177aa2cb973ef8caf
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-02-16 16:35:19 +0300

    start

---
+++ testiki/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace testiki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_select = new System.Windows.Forms.Button();
+            this.button_marks = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(175, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_SelectIndexChanged);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(338, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(495, 36);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(370, 227);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(338, 253);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.TabIndex = 4;
+            this.button_add.Text = "add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_select
+            // 
+            this.button_select.Location = new System.Drawing.Point(419, 253);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(75, 45);
+            this.button_select.TabIndex = 5;
+            this.button_select.Text = "select";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // button_marks
+            // 
+            this.button_marks.Location = new System.Drawing.Point(500, 253);
+            this.button_marks.Name = "button_marks";
+            this.button_marks.Size = new System.Drawing.Size(75, 23);
+            this.button_marks.TabIndex = 6;
+            this.button_marks.Text = "marks";
+            this.button_marks.UseVisualStyleBackColor = true;
+            this.button_marks.Click += new System.EventHandler(this.button_marks_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(316, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_marks);
+            this.Controls.Add(this.button_select);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.Button button_marks;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ testiki/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testiki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.Items.Add("1 - что");
+            listBox1.Items.Add("2 - нет");
+            listBox1.Items.Add("3 - да");
+
+            comboBox1.Items.Add("что нет");
+            comboBox1.Items.Add("Рахат           лукум");
+            comboBox1.Items.Add("тут выше пробелы ьтипа б . . .  . . .  . . . . . .  . . . . . . . . . . .");
+
+            comboBox1.SelectedIndex = 0;
+
+            checkedListBox1.Items.Add("Ну и кри   н   ж");
+            checkedListBox1.Items.Add("нормик тут так-то");
+            checkedListBox1.Items.Add("я тут ваще по приколу");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            string newCity = textBox1.Text;
+
+            if (newCity != "")
+            {
+                listBox1.Items.Add(newCity);
+
+                textBox1.Clear();
+
+                label1.Text = "Добавлен";
+            }
+            else label1.Text = "Введите город";
+        }
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                label1.Text = listBox1.SelectedItem.ToString();
+            else label1.Text = "Ничего";
+
+            if (comboBox1.SelectedIndex != -1)
+                label1.Text = comboBox1.SelectedItem.ToString();
+            else label1.Text = "Ничего";
+        }
+
+        private void button_marks_Click(object sender, EventArgs e)
+        {
+            string result = "Отмечено: \n";
+            for (int i = 0; i < listBox1.Items.Count; i++) {
+                if (checkedListBox1.GetItemChecked(i) == true) {
+                    result += checkedListBox1.Items[i].ToString() + '\n';
+                }
+            }
+            label1.Text = result;
+        }
+
+        private void listBox1_SelectIndexChanged(object sender, EventArgs e) { 
+            if(listBox1.SelectedIndex !=1)
+                label1.Text = "кликнул на " + listBox1.SelectedItem.ToString();
+        }
+    }
+}
